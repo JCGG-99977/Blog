eb4ad8e1bc9fee81a0bc7fa94cce620680d18946
JCGG-99977 <1848514604@qq.com>
2021-11-16 10:57:21 +0800

    初次修改

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,41 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <login/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import login from './views/login.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    login
   }
 }
 </script>
 
 <style>
+*{
+  margin: 0;
+  padding: 0;
+}
+html,body{
+  width: 100%;
+  height: 100%;
+}
+ul>li{
+  list-style: none;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  /* margin-top: 60px; */
+  height: 100%;
 }
 </style>
--- src/views/login.vue
+++ src/views/login.vue
@@ -1,10 +1,29 @@
 <template>
-  <div class="login"></div>
+  <div class="login">
+      <div class="content">
+          <ul>
+              <li>登录</li>
+              <li>注册</li>
+          </ul>
+          <div class="content_bottom">
+              
+          </div>
+      </div>
+  </div>
 </template>
 
 <script>
 export default {
+name:'login',
+data(){
+    return{
 
+    }
+},
+created(){},
+methods:{
+
+}
 }
 </script>
 
@@ -13,5 +32,30 @@ export default {
     width: 100%;
     height: 100%;
     background-color: aquamarine;
+    position: relative;
+}
+.content{
+    width: 400px;
+    height: 50%;
+    background-color: azure;
+    position: absolute;
+    top: 30%;
+    left: 60%;
+}
+.content ul{
+    height: 10%;
+    width: 100%;
+    display: flex;
+    line-height: 50px;
+}
+.content ul li{
+    float: left;
+    flex: 1;
+    background-color: bisque;
+}
+.content_bottom{
+    width: 100%;
+    height: 90%;
+    background-color:skyblue;
 }
 </style>
