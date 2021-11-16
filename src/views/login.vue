5a8c5efc2d69b5703c1f0fba07058eba6a6efda6
JCGG-99977 <1848514604@qq.com>
2021-11-16 10:39:26 +0800

    登录注册模块

---
+++ src/views/login.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="login"></div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.login{
+    width: 100%;
+    height: 100%;
+    background-color: aquamarine;
+}
+</style>
