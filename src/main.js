c3302ff5f66b3b985b95177328c7db90df2ca6ef
JCGG-99977 <1848514604@qq.com>
2021-11-17 10:41:03 +0800

    Vuex的引入

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 import router from './router'
+import vuex from 'vuex'
 
 import {
   post,
@@ -15,12 +16,19 @@ Vue.prototype.$post = post
 Vue.prototype.$get = get
 Vue.prototype.$put = put
 Vue.prototype.$delete = del
-
+Vue.use(vuex)
 Vue.use(ElementUI);
 
 Vue.config.productionTip = false
-
+// vuex
+const store = new vuex.Store({
+  // 存储用户的id
+  state: {
+    id: ''
+  },
+})
 new Vue({
   render: h => h(App),
-  router
+  router,
+  store
 }).$mount('#app')
